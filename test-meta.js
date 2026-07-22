3a172401967aa003f473c1baceba8472343829a1
ArthurSantosss <arthurvilas@gmail.com>
2026-07-22 11:22:55 -0300

    Update Meta token config

--- test-meta.js
+++ test-meta.js
@@ -1,4 +1,8 @@
-const token = 'EAAZAuTXAeZB3UBQ1xyMP5Hp8GRiBoZCxlXTVLaBLqI0EGDsEmsnLdHGbD6guoDYYZChKIBu3yKJVL0gqEuZBPay29u6RBOeiFjU8I8wMZCcbPzIlfpSVQbtl7d7v4ZCBb4YixCDwVyOhqYfVseb04CXVLNVNiHHQQlUAKe6PMFqd9x4vSC8kZA4iLqQ0O9oAzDbZC';
+const token = process.env.META_ACCESS_TOKEN || process.env.VITE_META_ACCESS_TOKEN;
+
+if (!token) {
+  throw new Error('Set META_ACCESS_TOKEN before running this script.');
+}
 
 async function run() {
   const url = `https://graph.facebook.com/v22.0/me/adaccounts?fields=id,name,balance,amount_spent,spend_cap,is_prepay_account,funding_source_details&access_token=${token}`;
